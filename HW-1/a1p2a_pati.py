d4d31d3f6f916a22c76392728cbccfe0f52819c0
dibya-pati <dibyajyoti.pati@stonybrook.edu>
2017-10-06 22:00:22 -0400

    modified a1p2a

--- HW-1/a1p2a_pati.py
+++ HW-1/a1p2a_pati.py
@@ -4,6 +4,7 @@ from pyspark.sql import SparkSession
 from pyspark import SparkConf, SparkContext
 import sys
 
+APP_NAME = "My Spark Application"
 
 
 # def create_spark_session(app_name="SparkApplication"):
@@ -22,16 +23,13 @@ class MyMapReduce:
 
     def __init__(self, data):  # [DONE]
         self.data = data  # the "file": list of all key value pairs
-        # Configure Spark
-        conf = SparkConf().setAppName(APP_NAME)
-        conf = conf.setMaster("local[*]")
-        self.sc = SparkContext(conf=conf)
+
 
 #WordCount implementation below:
 class WordCountMR(MyMapReduce):
 
     def countWords(self):
-        wordCountRDD = self.sc.parallelize(self.data)
+        wordCountRDD = sc.parallelize(self.data)
 ##removed word cleaning
 ##.map(lambda item: re.sub(r'(\w)[.:,]', r'\1', item)) \
         mapRet = wordCountRDD.map(lambda item:item[1].split())\
@@ -46,7 +44,7 @@ class WordCountMR(MyMapReduce):
 class SetDifferenceMR(MyMapReduce):
 
     def findDiffElements(self):
-        SetDifferenceRDD = self.sc.parallelize(self.data)
+        SetDifferenceRDD = sc.parallelize(self.data)
         mapRet = SetDifferenceRDD.flatMapValues(lambda x:x).map(lambda item:(item[1],item[0]))\
          .groupByKey().map(lambda item:(item[0],set(item[1])))\
          .filter(lambda item:list(item[1])[0]=='R' and len(item[1])==1).map(lambda item:item[0])
@@ -80,15 +78,15 @@ def main():
              ('S', ['pear', 'orange', 'strawberry', 'fig', 'tangerine'])]
     data2 = [('R', [x for x in range(50) if random() > 0.5]),
              ('S', [x for x in range(50) if random() > 0.75])]
-    WordCountMRObject.sc.stop()
     SetDifferenceMRObject1 = SetDifferenceMR(data1)
     SetDifferenceMRObject2 = SetDifferenceMR(data2)
     print((SetDifferenceMRObject1.findDiffElements().collect()))
     print((SetDifferenceMRObject2.findDiffElements().collect()))
-    SetDifferenceMRObject1.sc.stop()
-    SetDifferenceMRObject1.sc.stop()
 
 
 
 if __name__ == '__main__':
-    main()
+     conf = SparkConf().setAppName(APP_NAME)
+     conf = conf.setMaster("local[*]")
+     sc = SparkContext(conf=conf)
+     main()
